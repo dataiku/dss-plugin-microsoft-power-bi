07cd1a0f847ecf7d80863835759cab68b3ca82d6
Alexandre Bourret <alexandre.bourret@dataiku.com>
2020-09-15 15:03:35 +0200

    Specific error message for manually defined oauth

--- python-exporters/python-both-auth/exporter.py
+++ python-exporters/python-both-auth/exporter.py
@@ -1,12 +1,10 @@
 import json
-import logging
 from powerbi import PowerBI, generate_access_token
 from dataiku.exporter import Exporter
 from math import isnan
+from safe_logger import SafeLogger
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO,
-                    format='power-bi plugin %(levelname)s - %(message)s')
+logger = SafeLogger("power-bi-v2 plugin", forbiden_keys=["ms-oauth_credentials", "password", "client-secret"])
 
 
 class PowerBIExporter(Exporter):
@@ -14,6 +12,7 @@ class PowerBIExporter(Exporter):
     EMPTY_CONNECTION = {"username": None, "password": None, "client-id": None, "client-secret": None}
 
     def __init__(self, config, plugin_config):
+        logger.info("config={}, plugin_config={}".format(logger.filter_secrets(config), logger.filter_secrets(plugin_config)))
         self.config = config
         self.plugin_config = plugin_config
         self.row_index = 0
@@ -31,13 +30,8 @@ class PowerBIExporter(Exporter):
 
         authentication_method = self.config.get("authentication_method", None)
         if authentication_method == "oauth":
-            try:
-                access_token = config.get('powerbi_connection')['ms-oauth_credentials']
-                self.pbi = PowerBI(access_token)
-            except Exception as err:
-                logger.error("ERROR [-] Error while reading your Power BI access token from Project Variables")
-                logger.error(str(err))
-                raise Exception("Authentication error")
+            access_token = self.get_oauth_token_from_config(config)
+            self.pbi = PowerBI(access_token)
             self.pbi_group_id = self.pbi.get_group_id_by_name(self.pbi_workspace)
         elif authentication_method == "credentials":
             basic_connection = self.config.get("basic_connection", self.EMPTY_CONNECTION)
@@ -61,6 +55,17 @@ class PowerBIExporter(Exporter):
             # Interacting with Power BI API's
             self.pbi = PowerBI(token)
             self.pbi_group_id = self.pbi.get_group_id_by_name(self.pbi_workspace)
+
+    def get_oauth_token_from_config(self, config):
+        access_token = config.get('powerbi_connection', {}).get('ms-oauth_credentials')
+        if access_token is None:
+            logger.error("ERROR [-] Error while reading your Power BI access token from Project Variables")
+            raise Exception("Authentication error")
+        if isinstance(access_token, dict):
+            NO_INLINE_ERROR_MESSAGE = "OAuth settings cannot be used inlined. Please define a preset in Plugins > Microsoft Power BI v2 > Settings > Azure Single Sign On"
+            logger.error("ERROR [-] {}".format(NO_INLINE_ERROR_MESSAGE))
+            raise Exception("Authentication error. {}".format(NO_INLINE_ERROR_MESSAGE))
+        return access_token
 
     def open(self, schema):
         self.schema = schema
